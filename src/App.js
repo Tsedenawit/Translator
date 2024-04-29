38c0d25b92490dfc023fdcc5cd82d78c2c40e727
Tsedenawit <tsedenawit3@gmail.com>
2024-04-29 20:48:44 +0300

    send

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import './App.css';
 
 function App() {
   return (
-    <div className='flex'>
-    <div className='bg-red-800 w-[40%] mx-auto '>
-      <div className='flex text-white'>
+    <div className='flex justify-center'>
+    <div className='bg-red-800 w-[40%] mx-auto bg-gradient-to-l from-cyan-500 to-blue-500'>
+      <div className='flex text-white justify-between'>
       <button>Detect lang</button>
       <button>English</button>
       <button>French</button>
       <button>Spanish</button>
       </div>
+      {/* <form>
+        <textarea className='w-[600px] ml-2 '></textarea>
+      </form> */}
+
       </div>
+      
        <div className='bg-red-800 w-[40%] mx-auto '>
       <ul>
         <li>Detect Language</li>
